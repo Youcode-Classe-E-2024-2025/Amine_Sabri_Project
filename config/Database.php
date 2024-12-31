83d651fd464158b6d5d8de170f0b1b92d6cc0147
sabri-amine <sabriamine368@gmail.com>
2024-12-31 12:36:05 +0100

    j'ai ajoute structure mvc et ajouté conexion pdo avec database

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database {
+    private $pdo;
+    private $host = 'localhost';
+    private $dbname = 'projetTask';
+    private $username = 'root'; 
+    private $password = '';
+
+    public function __construct() {
+        try {
+            $this->pdo = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connexion réussie à la base de données !";
+        } catch (PDOException $e) {
+            echo "Erreur de connexion : " . $e->getMessage();
+        }
+    }
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
+?>
