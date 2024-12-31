621e6c5384cc8e6c80ec2f57389311b3fe0da093
sabri-amine <sabriamine368@gmail.com>
2024-12-31 16:54:46 +0100

    Ajouté connexion dans userController

--- controller/userController.php
+++ controller/userController.php
@@ -1,29 +1,53 @@
 <?php
-    require_once __DIR__ .'/../model/user.php';
+require_once __DIR__ . '/../model/user.php';
 
-    class UserController{
-        public function create(){
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                $name = $_POST["name"];
-                $email = $_POST["email"];
-                $password = $_POST["password"];
+class UserController {
+    public function create() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
 
-                if(empty(trim($name)) || empty(trim($name)) || empty(trim($password))){
-                    echo "rempli les inputs";
-                    return ;
-                }
-                $userModel = new User();
-                $UserRegester = $userModel->signUp($name,$email,$password);
-                if($UserRegester){
-                    echo 'bienvenue to login';
-                }else{
-                    echo 'back to signUp';
-                }
+            if (empty(trim($name)) || empty(trim($email)) || empty(trim($password))) {
+                echo "Remplis tous les champs.";
+                return;
+            }
 
+            $userModel = new User();
+            $UserRegistered = $userModel->signUp($name, $email, $password);
+
+            if ($UserRegistered) {
+                header("Location: ../views/sign/signIn.html");
+                exit;
+            } else {
+                header("Location: ../views/sign/signUp.html");
+                exit;
             }
         }
     }
 
-// $user = new UserController();
-// $user->create();
+    public function connexion() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+
+            if (empty(trim($email)) || empty(trim($password))) {
+                echo "Remplis tous les champs.";
+                return;
+            }
+
+            $userModel = new User();
+            $user = $userModel->signIn($email, $password);
+
+            if ($user) {
+                echo "Welcome to Home";
+                header("Location: ../index.php");
+                exit;
+            } else {
+                header("Location: ../views/sign/signIn.html");
+                exit;
+            }
+        }
+    }
+}
 ?>
