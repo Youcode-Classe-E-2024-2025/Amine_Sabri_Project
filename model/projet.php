3bcfd113623f9253492d0069d7fd0d82dda7b8ee
sabri-amine <sabriamine368@gmail.com>
2025-01-01 17:31:55 +0100

    j'ai ajouté modification  de projet

--- model/projet.php
+++ model/projet.php
@@ -22,6 +22,24 @@
             }
             echo "Project create";
         }
+        
+        
+        
+        public function update($name, $description, $userId, $visibility, $id_projet, $userIds) {
+                $sql = "UPDATE projects SET name = ?, description = ?, created_by = ?, visibility = ? WHERE id = ?";
+                $stmt = $this->db->prepare($sql);
+                $stmt->execute([$name, $description, $userId, $visibility, $id_projet]);
+                $sqlDeleteUsers = "DELETE FROM project_user WHERE project_id = ?";
+                $stmtDelete = $this->db->prepare($sqlDeleteUsers);
+                $stmtDelete->execute([$id_projet]);
+                $sqlUsers = "INSERT INTO project_user (project_id, user_id) VALUES (?, ?)";
+                $stmtUsers = $this->db->prepare($sqlUsers);
+        
+                foreach ($userIds as $user) {
+                    $stmtUsers->execute([$id_projet, $user]);
+                }
+                echo "Project modifier ";
+        }
     }
 
 ?>
