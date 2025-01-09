99fc614947f4e58509f9c8ff5a069eceab57d4d0
sabri-amine <sabriamine368@gmail.com>
2025-01-09 15:23:09 +0000

    ajouté vérification dans les projet pour edit ou modification

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -37,48 +37,62 @@ if (isset($_SESSION['message'])) {
     }
     ?>
 
-        <section>
-            <div class="container mx-auto p-6">
-                <h1 class="text-5xl font-bold text-center mb-8">Projects Showcase</h1>
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <?php
-                    $projects = Projet::getAllProjet();
-            
-                    if (!empty($projects)) {
-                        foreach ($projects as $project) {
-                            $visibilityColor = ($project['visibility'] == 'private') ? 'text-red-600' : 'text-green-600';
-                            
-                            echo "
-                            <div class='bg-white border border-gray-300 shadow-lg rounded-lg overflow-hidden transform hover:scale-105 hover:shadow-xl transition duration-300'>
-                                <a href='./todo.php?id=" . $project['id'] . "'>
-                                    <div class='bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 p-2'>
-                                        <h2 class='text-xl font-bold mb-1 text-white'>{$project['name']}</h2>
-                                    </div>
-                                    <div class='p-4'>
-                                        <p class='text-gray-700 mb-2'>
-                                            <strong>Visibility:</strong> <span class='{$visibilityColor}'>" . ucfirst($project['visibility']) . "</span>
-                                        </p>
-                                        <p class='text-gray-700 mb-2'>
-                                            <strong>Description:</strong> {$project['description']}
-                                        </p>
-                                        <div class=''>
-                                            <a href='editProject.php?id={$project['id']}' class='text-indigo-500 py-1 px-3 rounded-full hover:text-indigo-700 transition duration-300'><i class='bi bi-pencil-square'></i></a>
-                                            <form action='http://localhost/amine_Sabri_Project/index.php?action=deleteProjet' method='POST' class='inline'>
-                                                <input type='hidden' name='id' value='{$project['id']}'>
-                                                <button type='submit' class='text-green-500 py-1 px-3 rounded-full hover:text-green-700 transition duration-300'><i class='bi bi-trash-fill'></i></button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </a>    
-                            </div>";    
-                        }
-                    } else {
-                        echo "<p class='text-center text-gray-500'>No projects available at the moment.</p>";
-                    }
-                    ?>
-                </div>
-            </div>
-        </section>
+<section>
+    <div class="container mx-auto p-6">
+        <h1 class="text-5xl font-bold text-center mb-8">Projects Showcase</h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <?php
+            $projects = Projet::getAllProjet();
+    
+            if (!empty($projects)) {
+                foreach ($projects as $project) {
+                    $visibilityColor = ($project['visibility'] == 'private') ? 'text-red-600' : 'text-green-600';
+                    
+                    echo "
+                    <div class='bg-white border border-gray-300 shadow-lg rounded-lg overflow-hidden transform hover:scale-105 hover:shadow-xl transition duration-300'>
+                        <a href='./todo.php?id=" . $project['id'] . "'>
+                            <div class='bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 p-2'>
+                                <h2 class='text-xl font-bold mb-1 text-white'>{$project['name']}</h2>
+                            </div>
+                            <div class='p-4'>
+                                <p class='text-gray-700 mb-2'>
+                                    <strong>Visibility:</strong> <span class='{$visibilityColor}'>" . ucfirst($project['visibility']) . "</span>
+                                </p>
+                                <p class='text-gray-700 mb-2'>
+                                    <strong>Description:</strong> {$project['description']}
+                                </p>
+                                <div class=''>";
+                                
+                                if ($auth->hasPermission('update_project')) {
+                                    echo "
+                                    <a href='editProject.php?id={$project['id']}' class='text-indigo-500 py-1 px-3 rounded-full hover:text-indigo-700 transition duration-300'>
+                                        <i class='bi bi-pencil-square'></i>
+                                    </a>";
+                                }
+                                
+                                if ($auth->hasPermission('delete_project')) {
+                                    echo "
+                                    <form action='http://localhost/amine_Sabri_Project/index.php?action=deleteProjet' method='POST' class='inline'>
+                                        <input type='hidden' name='id' value='{$project['id']}'>
+                                        <button type='submit' class='text-green-500 py-1 px-3 rounded-full hover:text-green-700 transition duration-300'>
+                                            <i class='bi bi-trash-fill'></i>
+                                        </button>
+                                    </form>";
+                                }
+                                
+                    echo "
+                                </div>
+                            </div>
+                        </a>    
+                    </div>";
+                }
+            } else {
+                echo "<p class='text-center text-gray-500'>No projects available at the moment.</p>";
+            }
+            ?>
+        </div>
+    </div>
+</section>
 
 
 
